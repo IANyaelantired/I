573e233ad4e26206f1f6fe1175b5f4a2bd395082
IANyaelantired <94999856+IANyaelantired@users.noreply.github.com>
2022-01-10 16:59:17 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+
+
+
+//Crear espacio de nombre para Engine
+const Engine = Matter.Engine;
+//Crear espacio de nombre para World
+const World = Matter.World;
+//Crear espacio de nombre para Bodies
+const  Bodies = Matter.Bodies;
+//Crear espacio de nombre para Body
+const Body = Matter.Body;
+
+
+function setup() {
+  createCanvas(400,400);
+//crear engine
+engine = Engine.create();
+world = engine.world;
+
+
+  //Agregar world a engine
+  
+
+  
+   var ball_options = {
+    restitution: 7,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+  
+//crear un fondo
+//agregarlo a world
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  
+  ground = Bodies.rectangle(200,390,400,20, ground_options);
+
+  World.add(world, ground);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+
+  ellipse(ball.position.x,ball.position.y,20);
+  //escribir una función rectangle para mostrar el suelo.
+
+  rect(ground.position.x, ground.position.y,400,20);
+ 
+
+
+  
+  
+}
+
